899442df8ee5d267e4c9eb63485f7b3894be5c25
matthew-coudert-cko <matthew.coudert@checkout.com>
2024-01-08 12:50:01 +0000

    reimport List and ArrayList

--- src/main/java/com/snowflake/kafka/connector/records/RecordService.java
+++ src/main/java/com/snowflake/kafka/connector/records/RecordService.java
@@ -26,6 +26,8 @@ import java.math.BigDecimal;
 import java.nio.ByteBuffer;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
